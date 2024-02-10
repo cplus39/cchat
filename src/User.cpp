bc9c9ac47a496c9ba137257d6f1e4a9011e9ce4c
artem vasenko <artemvasenko09@mail.ru>
2024-02-10 17:37:27 +0300

    Дописал: printUserMenu

--- src/User.cpp
+++ src/User.cpp
@@ -1,11 +1,14 @@
 #include "User.hpp"
+#include "Chat.hpp"
 
 void User::setPassword(std::string& login)
 {
+
 }
 
 void User::setName(std::string& name)
 {
+
 }
 
 std::string const &User::getLogin()
